9c0ca262b81ee623b1b5a20c5e63281edd99ecbc
Bhavanbir S. Dhandi <117181972+bhavanbir@users.noreply.github.com>
2025-12-19 15:38:19 -0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,15 @@
-
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#56BDFF",
+        dark: "#323232"
+      }
+    }
+  },
+  plugins: [],
+};
